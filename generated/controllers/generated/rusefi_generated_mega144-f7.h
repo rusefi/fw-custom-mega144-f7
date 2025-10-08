42ade11886a94ed6f1910a033ce8217247276f40
GitHub git update Action <action@github.com>
2025-10-08 00:19:52 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega144-f7.h
+++ generated/controllers/generated/rusefi_generated_mega144-f7.h
@@ -2103,7 +2103,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.10.07.mega144-f7.1479152566"
+#define TS_SIGNATURE "rusEFI main.2025.10.08.mega144-f7.1479152566"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
