a0b81a2b55616a34b1ee9ea9fcbb03b2628ad373
GitHub git update Action <action@github.com>
2026-05-17 00:40:37 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_mega144-f7.h
+++ generated/controllers/generated/signature_mega144-f7.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2863486911
-#define TS_SIGNATURE "rusEFI main.2026.05.16.mega144-f7.2863486911"
+#define SIGNATURE_HASH 693302702
+#define TS_SIGNATURE "rusEFI main.2026.05.17.mega144-f7.693302702"
