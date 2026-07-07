d3ec82892ccf143f6c511dec3e059f268ee7ee29
GitHub git update Action <action@github.com>
2026-07-07 00:40:32 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega144-f7.h
+++ generated/controllers/generated/rusefi_generated_mega144-f7.h
@@ -1610,7 +1610,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2864373823
+#define SIGNATURE_HASH 2392157793
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2237,7 +2237,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.07.06.mega144-f7.2864373823"
+#define TS_SIGNATURE "rusEFI main.2026.07.07.mega144-f7.2392157793"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_mega144-f7.h
+++ generated/controllers/generated/signature_mega144-f7.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2864373823
-#define TS_SIGNATURE "rusEFI main.2026.07.06.mega144-f7.2864373823"
+#define SIGNATURE_HASH 2392157793
+#define TS_SIGNATURE "rusEFI main.2026.07.07.mega144-f7.2392157793"
